73768153a99f99d3a98e61ef88cec63d64b54436
Arisha Ahmed <96740259+Arisha110@users.noreply.github.com>
2024-05-23 16:26:50 +0530

    few more changes

--- client/src/components/Home/HeroSliderCard.jsx
+++ client/src/components/Home/HeroSliderCard.jsx
@@ -5,7 +5,7 @@ function HeroSliderCard({ title, img }) {
   const navigate = useNavigate()
   return (
     <div>
-      <div className='flex justify-between sm:items-center items-start my-2 gap-1 py-6 heroSlider'>
+      <div className='flex justify-between px-5 sm:items-center items-start my-2 gap-1 py-6 heroSlider'>
         <div className='flex flex-col gap-4 items-center lg:px-24 md:px-4 justify-center w-1/2'>
           <h1 className='md:text-4xl sm:text-2xl text-lg text-left sm:font-bold font-semibold text-white font-mono'>{title}</h1>
           <button className='w-24 text-orange-600 hover:bg-gray-200 font-semibold py-2 sm:w-full bg-white rounded-md' onClick={() => navigate("/provider")}>Book Your Tiffin</button>
